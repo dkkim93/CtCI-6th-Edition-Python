0357388f832aafebb9ce7b71141c78518403c7a2
Dong Ki Kim <dkkim93@mit.edu>
2019-08-19 19:28:25 -0400

    Added comments to solution

--- Chapter1/1_Is Unique/IsUnique.py
+++ Chapter1/1_Is Unique/IsUnique.py
@@ -5,11 +5,11 @@ import unittest
 def unique(string):
     # Assuming character set is ASCII (128 characters)
     if len(string) > 128:
-        return False
+        raise ValueError("Max string limit is exceeded")
 
     char_set = [False for _ in range(128)]
     for char in string:
-        val = ord(char)
+        val = ord(char)  # Convert Unicode char into int
         if char_set[val]:
             # Char already found in string
             return False
@@ -31,6 +31,7 @@ class Test(unittest.TestCase):
         for test_string in self.dataF:
             actual = unique(test_string)
             self.assertFalse(actual)
+
 
 if __name__ == "__main__":
     unittest.main()
